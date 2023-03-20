19df934c03f4319bb94fb0313f8f7461d16a08ef
sardor <alimukhammad1@gmail.com>
2023-03-19 21:11:36 -0400

    1st simple functioning calculator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'calculator_simple_ag';
+
+  
+  public num1: number;
+  public num2: number;
+  // public num3: number = 0;
+  public result: number;
+
+  constructor() {
+    this.num1 = 0;
+    this.num2 = 0;
+    this.result = 0;
+    
+  }
+
+  add(){
+    this.result = this.num1 + this.num2;
+  }
 }
